61fb2bc43c52ba936f2eada1440a0903551bb50b
Rod Machen <mail@rodmachen.com>
2015-09-11 22:31:35 -0500

    Implement database connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,38 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+module.exports = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'chat'
+});
 
+module.exports.connect();
+
+module.exports.query('SELECT * from messages', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+module.exports.end();
+
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '',
+//   database : 'chat'
+// });
+
+// connection.connect();
+
+// connection.query('SELECT * from messages', function(err, rows, fields) {
+//   if (!err)
+//     console.log('The solution is: ', rows);
+//   else
+//     console.log('Error while performing Query.');
+// });
+
+// connection.end();
