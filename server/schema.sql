df5e3b9a06ce88282dbe572a1b2851ca5efa40fa
Rod Machen <mail@rodmachen.com>
2015-09-11 22:30:19 -0500

    Implement schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,31 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS rooms;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  msgID INT(4), userID INT(4) DEFAULT 1, roomID INT(4) DEFAULT 1, text VARCHAR(140)
 );
+
+CREATE TABLE users (
+  userID INT(4), username VARCHAR(30) DEFAULT 'anonymous'
+);
+
+CREATE TABLE rooms (
+  roomID INT(4), roomname VARCHAR(30) DEFAULT 'lobby'
+);
+
+INSERT INTO messages VALUES (1, DEFAULT, DEFAULT, 'Hello there!');
+INSERT INTO messages VALUES (2, DEFAULT, DEFAULT, 'Hey there!');
+
+INSERT INTO users VALUES (1, 'anonymous!');
+
+INSERT INTO rooms VALUES (1, 'lobby!');
 
 /* Create other tables and define schemas for them here! */
 
